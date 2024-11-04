9833cebe195fc3aafb221ce226d90593b29f53f9
Mykola-Tylyk <tylyk.m.v@gmail.com>
2024-11-04 23:24:58 +0200

    ДЗ 1

---
+++ HW_1/hw-1.2/hw-1.2.js
@@ -0,0 +1,14 @@
+let firstName = 'Mykola';
+let middleName = 'Volodymyrovych';
+let lastName = 'Tylyk';
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
+
+
+
+
+
+
+
+
+
