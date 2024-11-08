248eb14f8602d613f7e064758018231e8646e871
Mykola-Tylyk <tylyk.m.v@gmail.com>
2024-11-08 21:26:13 +0200

    ДЗ 2

--- HW_2/hw-2.12/hw-2.12.js
+++ HW_2/hw-2.12/hw-2.12.js
@@ -8,19 +8,19 @@ let coursesAndDurationArray = [
 ];
 if (coursesAndDurationArray[0].monthDuration > 5) {
     console.log('Супер')
-};
+}
 if (coursesAndDurationArray[1].monthDuration > 5) {
     console.log('Супер')
-};
+}
 if (coursesAndDurationArray[2].monthDuration > 5) {
     console.log('Супер')
-};
+}
 if (coursesAndDurationArray[3].monthDuration > 5) {
     console.log('Супер')
-};
+}
 if (coursesAndDurationArray[4].monthDuration > 5) {
     console.log('Супер')
-};
+}
 if (coursesAndDurationArray[5].monthDuration > 5) {
     console.log('Супер')
 }
